ff7e4b320587bb8805d8df461993868f17b84aaa
Jesper Andersen <jesperandersen@localhost.localdomain>
2014-09-02 21:58:13 +0200

    Added incorrect version of computeMatches function
    Also: added TODO list

--- ast.js
+++ ast.js
@@ -63,7 +63,14 @@ var AST = (function(){
 	return term.op && term.op === "meta";
     } 
 
+    function isTerm(term) {
+	return term.op;
+    }
+
     function equalsTerm(t1, t2) {
+	if(!isTerm(t1) || !isTerm(t2)) {
+	    return t1 === t2;
+	}
         if (t1.op !== t2.op) {
             return false;
         }
@@ -71,12 +78,22 @@ var AST = (function(){
             return false;
         }
         
-        for(var i in t1.args) {
-            if(!equalsTerm(t1.args[i], t2.args[i])) {
+        for(var i in t1.elems) {
+            if(!equalsTerm(t1.elems[i], t2.elems[i])) {
                 return false;
             }
         }
         return true;
+    }
+
+    // lookup 'meta' in 'sub'
+    function lookupMeta(meta, sub) {
+	return sub[meta.elems[0]];
+    }
+
+    // updates 'sub' to have a binding from 'meta' to 'term'
+    function bindTerm(meta,term,sub){
+	sub[meta.elems[0]] = term;
     }
 
     // Apply substitution in 'pattern' with bindings from 'sub'
@@ -96,19 +113,88 @@ var AST = (function(){
 
     // Compute a substitution from meta-variables in 'pattern' such that
     // applyPattern pattern subst = tree 
-    function matches(pattern, tree) {
+    function computeMatches(pattern, tree) {
+	function match(pattern, tree, sub) {
+	    // X , term
+	    if(isMeta(pattern)){
+		var bound = lookupMeta(pattern, sub);
+		if(!bound) {
+		    bindTerm(pattern,tree,sub);
+		    return;
+		} else {
+		    console.log("meta bound");
+		    console.log(pattern);
+		    console.log(tree);
+		    console.log(bound);
+		    if(!equalsTerm(bound, tree)) {
+			console.log("should bail out now");
+			sub = null;
+			return;
+		    } else {
+			console.log("equals?");
+			console.log(bound);
+			console.log(tree);
+			return;
+		    }
+		}
+	    } else {
+		if(equalsTerm(pattern, tree)) {
+		    return;
+		} else {
+		    // op(elems1), op(elems2) && |elems1| = |elems2|
+		    if(pattern.op === tree.op && 
+		       pattern.elems && tree.elems &&
+		       pattern.elems.length == tree.elems.length) {
+			for(var i in pattern.elems) {
+			    match(pattern.elems[i], tree.elems[i], sub);
+			}
+			console.log("after for");
+			console.log(sub);
+		    } else {
+			sub = null;
+			return;
+		    }
+		}
+	    }
+
+	}
+	var sub = {}
+	match(pattern, tree, sub);
+	return sub;
     }
     
     return {
         mk: makeTerm,
         equals: equalsTerm,
-	applyPattern: applyPattern
+	applyPattern: applyPattern,
+	computeMatches: computeMatches
     };
 })();
 
-var term1 = AST.mk("num", [1]);
-
+var term1 = AST.mk("num", [42]);
+var term2 = AST.mk("num", [117]);
+var meta1 = AST.mk("meta", [0]);
+var meta2 = AST.mk("meta", [1]);
 // f(<meta-1>) {meta-1 : "1"} = f(1)
 var test1 = AST.applyPattern(AST.mk("call",[AST.mk("id",["f"]), AST.mk("argList",[{op:"meta",elems:[1]}])]), {1:term1});
-var f1 = AST.mk("call", [AST.mk("id",["f"]), term1]);
+var f1 = AST.mk("call", [AST.mk("id",["f"]), term1, term2]);
+var p1 = AST.mk("call", [AST.mk("id",["f"]), meta1, meta1]);
+
 var test1res = AST.equals(test1, f1)
+
+console.log("loaded");
+
+console.log(AST.computeMatches(p1, f1));
+console.log("done");
+/*
+  TODO
+
+  - implement hash-consing for terms for constant time equality checks
+
+  - make tree traversal more generic so we don't rely on concrete
+    implementation externally
+
+  - modularize into several modules; patterns do not belong in AST
+
+  - write tests
+*/
