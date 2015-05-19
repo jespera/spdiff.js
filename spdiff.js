b4679bfb4e6a604760d80aa433c7d82098d58c09
jespera <jespera@gmail.com>
2015-05-19 20:31:42 +0200

    Better conversion of term to AST

--- spdiff.js
+++ spdiff.js
@@ -39,7 +39,7 @@ function makeTerm(op, elems) {
 				console.log("something is wrong with elems: " + JSON.stringify(elems,null,2))
 				throw "Oh no!";
 		}
-		var termHash = 
+		var termHash =
 				elems.reduce(function(accHash, elem){
 						var elemHash;
 						if(isTerm(elem)) {
@@ -49,7 +49,7 @@ function makeTerm(op, elems) {
 						}
 						var newHash = ((accHash<<5)-accHash)+elemHash;
 						return Math.abs(newHash & newHash);
-						
+
 				}, opHash);
 		var existingTerm = termsMemo[termHash];
 		if(existingTerm){
@@ -58,7 +58,7 @@ function makeTerm(op, elems) {
 		}
     var newTerm = { op: op, elems: elems, hkey: termHash};
 		//TODO: somehow get weak references here
-		termsMemo[termHash] = newTerm; 
+		termsMemo[termHash] = newTerm;
 		return newTerm;
 }
 
@@ -72,7 +72,7 @@ function makeMeta() {
 
 function isMeta(term) {
 		return term && term.op && term.op === "meta";
-} 
+}
 
 function equalsTerm(t1, t2) {
 		if(!isTerm(t1) || !isTerm(t2)) {
@@ -112,13 +112,13 @@ function applyPattern(pattern, sub) {
 
 function areComparable(tree1, tree2) {
 		return tree1 && tree2
-				&& tree1.op === tree2.op 
-				&& tree1.elems && tree2.elems 
+				&& tree1.op === tree2.op
+				&& tree1.elems && tree2.elems
 				&& tree1.elems.length === tree2.elems.length
 }
 
 // Compute a substitution from meta-variables in 'pattern' such that
-// applyPattern pattern subst = tree 
+// applyPattern pattern subst = tree
 function computeMatches(pattern, tree) {
 		var env = {};
 		function match(pattern, tree) {
@@ -141,7 +141,7 @@ function computeMatches(pattern, tree) {
 
 				// pattern is comparable to tree, recurse on each subtree
 				// pattern is non-comparable, fail
-				if(areComparable(pattern, tree) ) 
+				if(areComparable(pattern, tree) )
 				{
 						for(var i in pattern.elems) {
 								match(pattern.elems[i], tree.elems[i]);
@@ -200,13 +200,13 @@ function mergeTerms(tree1, tree2, startEnv) {
 				if(typeof boundMeta !== undefined && boundMeta != null) {
 						return isDone(boundMeta);
 				}
-				
+
 				var newMeta = makeMeta();
 				env[[getHKey(t1),getHKey(t2)]] = newMeta;
-				
+
 				return isDone(newMeta);
 		}
-		
+
 		var visitor = traverse2(merge);
 		return visitor(tree1, tree2);
 }
@@ -239,8 +239,8 @@ function applyRewrite(patch, origTree) {
 				}
 
 				return tree;
-				
-				
+
+
 		}
 		return apply(origTree);
 }
@@ -258,7 +258,7 @@ function treeSize(tree) {
 		}
 		size(tree);
 		return accSize;
-		
+
 }
 
 function eqIs0(l, r) {
@@ -283,25 +283,25 @@ function editDist(oldT, newT) {
 				}
 				// both oldElems and newElems are non-null and have a
 				// length > 0
-				var min1 = 
-						distTerms(oldElems.slice(1), 
+				var min1 =
+						distTerms(oldElems.slice(1),
 											newElems)
 						+ treeSize(oldElems[0]);
-				var min2 = 		
-						distTerms(oldElems, 
+				var min2 =
+						distTerms(oldElems,
 											newElems.slice(1))
 						+ treeSize(newElems[0]);
 
 				var min3 =
 						distTerms(oldElems.slice(1),
-											newElems.slice(1)) 
-						+ dist(oldElems[0], 
+											newElems.slice(1))
+						+ dist(oldElems[0],
 									 newElems[0]);
-				
+
 				var res = Math.min(min1,min2,min3);
 				return res;
 		}
-		
+
 		function dist(o,n) {
 				var ohkey = getHKey(o);
 				var nhkey = getHKey(n);
@@ -337,7 +337,7 @@ function print(origTerm) {
 		function toStr(term) {
 				if(isMeta(term)) {
 						str += "X" + JSON.stringify(term.elems[0]);
-						
+
 				} else if(isTerm(term)) {
 						str += term.op;
 						if(term.elems && term.elems.length > 0) {
@@ -348,12 +348,12 @@ function print(origTerm) {
 												str += ", ";
 										}
 								}
-								str += ")"; 
+								str += ")";
 						}
 				} else {
 						str += JSON.stringify(term);
-				} 
-				
+				}
+
 		}
 		toStr(origTerm);
 		return str;
@@ -445,7 +445,7 @@ function getSimpleDiffs(srcTerm, tgtTerm) {
 }
 
 // we'll assume both lhsRewrite and rhsRewrite are safe for the changeset given
-// in other words, both rewrites applies to all pairs in the changeset 
+// in other words, both rewrites applies to all pairs in the changeset
 function isSubRewrite(changeset) { // changeset = [ {oldTerm:term,newTerm:term} ]
 		return function(lhsRewrite, rhsRewrite) {
 				return changeset.every(function(change) {
@@ -476,11 +476,11 @@ function getSubTerms(term) {
 				}
 				if(isUndef(subtermSet[key])) {
 						subtermSet[key] = nextTerm;
-				} 
+				}
 		}
-		
+
 		loop(term);
-		
+
 		var subterms = [];
 		for(var key in subtermSet) {
 				if(subtermSet.hasOwnProperty(key)) {
@@ -525,26 +525,38 @@ function getCommonPatterns(lhss) {
 function getMergeDiffs(changeset) {
 		var lhss = changeset.map(function(pair) { return pair.oldTerm; });
 		var commonPats = getCommonPatterns(lhss);
-		commonPats.forEach(function(pat) {
-				console.log(">>> " + print(pat));
-		});
+		// commonPats.forEach(function(pat) {
+		// 		console.log(">>> " + print(pat));
+		// });
 		console.log("computing pair diffs");
 		var simpleDiffs = changeset.map(function(change) {
-				// somewhat slow 
+				// somewhat slow
 				return getSimpleDiffs(change.oldTerm, change.newTerm)
 						.filter(function(change) {
-								return commonPats.some(function(pat) {
+							// console.log("testing simple: " + printRewrite(change));
+							var isGood = commonPats.some(function(pat) {
 										return computeMatches(pat, change.lhs);
 								});
+							// console.log(" good? " + isGood);
+							return isGood;
 						});
-				
+
 				// faster, but fewer results
 				//return getMinimalDiffs(change.oldTerm, change.newTerm);
 		});
+
+		console.log("no simple diffs = " + simpleDiffs.reduce(function(accSum, diffs) {
+			return diffs.length + accSum;
+			}, 0));
+		// simpleDiffs.forEach(function(diffs) {
+		// 	diffs.forEach(function (diff) {
+		// 		console.log("diff: " + printRewrite(diff));
+		// 	});
+		// });
 		var mergedChanges = [];
 
 
-		
+
 		function mergeFold(curMerge, otherDiffs) {
 				if(otherDiffs.length == 0) {
 						// check safety
@@ -554,13 +566,13 @@ function getMergeDiffs(changeset) {
 						if(!newMergeSafe) {
 								return;
 						}
-						
+
 						mergedChanges.push(curMerge);
 				} else {
 						var rewrites = otherDiffs[0];
 						rewrites.forEach(function (rewrite) {
 								var newMerge = curMerge ? mergeRewrites(curMerge, rewrite) : rewrite;
-								
+
 								if(newMerge && newMerge.lhs && isMeta(newMerge.lhs)) {
 										return;
 								}
@@ -618,7 +630,9 @@ function convertToTerm(ast) {
 }
 
 function convertToAST(term) {
-    if(!term) {
+    if(term == null) {
+			console.log("no term");
+			console.log(term);
         throw "No term?";
     }
     if(typeof(term) !== "object") {
@@ -633,7 +647,7 @@ function convertToAST(term) {
         }
 
         if(isMeta(term)) {
-						return "<" + term.elems[0] + ">";
+						return "X" + term.elems[0];
 				}
 
         var ast = {};
@@ -641,18 +655,25 @@ function convertToAST(term) {
 				if(term.elems.length === 1 && term.elems[0].op === "[]") {
 						return convertToAST(term.elems[0]);
 				}
-				
+
         for(var elemIdx in term.elems) {
+
             var termValue = term.elems[elemIdx];
-						
+
             var prop = termValue.op;
             if(termValue.elems.length !== 1) {
-                throw "Not === 1";
-            }
-            if(termValue.elems.length === 0) {
-                throw "=== 0?";
-            }
-            ast[prop] = convertToAST(termValue.elems[0]);            
+							// console.log("not === 1");
+							// console.log(termValue);
+							// console.log(">>> IN :");
+							// console.log(term);
+              // throw "Not === 1";
+							ast[prop] = convertToAST(termValue.elems);
+            } else {
+							ast[prop] = convertToAST(termValue.elems[0]);
+						}
+            // if(termValue.elems.length === 0) {
+              //  throw "=== 0?";
+            // }
         }
         return ast;
     }
@@ -673,20 +694,22 @@ spdiff.tester =
 				}
 
 				console.log("converting to internal tree-structure");
-				
+
 				var convChangeSet = changeset.map(convert);
 
 				console.log("merging diffs");
-				
+
 				var termRewrites = getMergeDiffs(convChangeSet);
-				
+
 
 				// var conv_changeset = [{oldTerm: f1_old_conv, newTerm: f1_new_conv},
 				// 											{oldTerm: f2_old_conv, newTerm: f2_new_conv}];
 				// var conv_rewrites = getMergeDiffs(conv_changeset);
-				
-				console.log("rewrites:" + termRewrites.length);
 
+				console.log("rewrites:" + termRewrites.length);
+				// termRewrites.forEach( function(rw) {
+				// 	console.log("rw:: " + printRewrite(rw));
+				// });
 				if(outputNode) {
 						outputNode.innerHTML = "";
 						if(termRewrites) {
@@ -702,6 +725,7 @@ spdiff.tester =
 												diff += "<br/>";
 												outputNode.innerHTML += diff;
 										} catch(err) {
+											console.log("failed to conv: " + printRewrite(rewrite));
 										}
 								});
 						} else {
@@ -733,6 +757,6 @@ if(debugging) {
 
 		var changeset = [{oldTerm: lhs1, newTerm: rhs1},
 										 {oldTerm: lhs2, newTerm: rhs2}];
-		
+
 		spdiff.tester(changeset);
 }
